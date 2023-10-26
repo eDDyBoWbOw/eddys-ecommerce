077279dda85018f3b68493da1e236e2386904880
edward caldwell <caldwell_edward12@yahoo.com>
2023-10-26 15:56:22 -0700

    finished models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: [10, 2], // Specify the total and decimal places as arguments
+          msg: 'price must be a valid decimal with 10 total digits and 2 decimal places',
+        },
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          msg: 'Stock must be a numeric value',
+        },
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
